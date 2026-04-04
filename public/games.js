52a596b945c6160e5d93dedc0e2e850a744b6fb0
Everest <>
2026-04-04 07:43:31 -0700

    Add agar.io; open real-time multiplayer games direct (not proxied)

--- public/games.js
+++ public/games.js
@@ -49,7 +49,9 @@
     { name: "Minesweeper",    url: "https://minesweeperonline.com",                        emoji: "💣", hue: 0   },
     { name: "Wordle",         url: "https://www.nytimes.com/games/wordle/index.html",      emoji: "🟩", hue: 130 },
     { name: "Chess",             url: "https://www.chess.com/play/online",                        emoji: "♟",  hue: 45  },
-    { name: "Krunker.io",        url: "https://krunker.io",                                       emoji: "🔫", hue: 200 },
+    { name: "Agar.io",           url: "https://agar.io",                                          emoji: "🔵", hue: 200, direct: true },
+    { name: "Krunker.io",        url: "https://krunker.io",                                       emoji: "🔫", hue: 200, direct: true },
+    { name: "Shell Shockers",    url: "https://shellshock.io",                                    emoji: "🥚", hue: 200, direct: true },
     { name: "Smash Karts",       url: "https://www.crazygames.com/game/smash-karts",              emoji: "🏎",  hue: 0   },
     { name: "Stickman Hook",     url: "https://www.crazygames.com/game/stickman-hook",            emoji: "🕷",  hue: 220 },
     { name: "Getaway Shootout",  url: "https://www.crazygames.com/game/getaway-shootout",         emoji: "🏃", hue: 15  },
@@ -61,7 +63,7 @@
     { name: "Color Tunnel",      url: "https://www.crazygames.com/game/color-tunnel",             emoji: "🌈", hue: 280 },
     { name: "Friday Night Funkin", url: "https://www.crazygames.com/game/friday-night-funkin",   emoji: "🎤", hue: 340 },
     { name: "Fireboy & Watergirl 1", url: "https://www.coolmathgames.com/0-fireboy-and-watergirl-1", emoji: "🔥", hue: 15 },
-    { name: "Shell Shockers",    url: "https://shellshock.io",                                    emoji: "🥚", hue: 200 },
+
     { name: "Vortelli's Pizza",  url: "https://www.crazygames.com/game/vortellis-pizza",          emoji: "🍕", hue: 10  },
     { name: "Stickman Warriors", url: "https://www.crazygames.com/game/stickman-warriors",        emoji: "🥷", hue: 0   },
     { name: "Penalty Shooters 2",url: "https://www.crazygames.com/game/penalty-shooters-2",      emoji: "⚽", hue: 100 },
@@ -198,8 +200,9 @@
     }
 
     tile.addEventListener("click", () => {
-      if (isLocal) { navigateLocal(game.url); }
-      else         { navigateTo(game.url); }
+      if (isLocal)        { navigateLocal(game.url); }
+      else if (game.direct) { window.open(game.url, "_blank"); }
+      else                { navigateTo(game.url); }
     });
     return tile;
   }
